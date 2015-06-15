826cda45aa08a1cd6cfc7c9f92293ed5212e737f
priyaman <priyaman@buffalo.edu>
2015-06-15 17:59:56 -0500

    once more

--- src/main/java/com/fedex/priyaman/login/LoginServlet.java
+++ src/main/java/com/fedex/priyaman/login/LoginServlet.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class LoginServlet
  */
-//@WebServlet("/LoginServlet")
+@WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
